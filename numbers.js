26483053b6767b33422806fa3fbd9dc1541041e9
Nathan Caza <mastercactapus@gmail.com>
2012-03-07 17:32:33 -0600

    first draft of number-to-word function

--- numbers.js
+++ numbers.js
@@ -16,37 +16,59 @@ exports.stringSum = function(num1,num2){
 }
 
 exports.getDigit = function(n,digit){
+	n=parseInt(n);
 	return (Math.floor(n/(Math.pow(10,digit-1))))%10
+}
+exports.getDigitS = function(n,digit){ //todo
+	n=n.toString();
+
+}
+exports.tripletName = function (n){
+	var words = "Zero One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen Twenty Thirty Fourty Fifty Sixty Seventy Eighty Ninety Hundred".split(" ");
+	n = parseInt(n);
+	var h=exports.getDigit(n,3),
+		t=exports.getDigit(n,2),
+		o=exports.getDigit(n,1);
+	if (t+h+o == 0) return words[0];
+	var out="";
+	if (h>0) {out += words[h] + " hundred" + ((t+o > 0) ? ' and':'')};
+	
+	if (t>1) {out += " " + words[t + 18] + ((o>0) ? '-' + words[o] : '');}
+	else if (t == 1) {out += " " + words[o+10];}
+	else if (o>0){out += " " + words[o];};
+	return out.trim();
 }
 
 exports.toWords= function (n){
-	var words = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty thirty fourty fifty sixty seventy eighty ninety hundred thousand million billion trillion".split();
-	var data = n.toString();
-	var groups = Math.ceil(data.length/3);
-	var offset = data.length % 3;
+	var suffix = "Thousand Million Billion Trillion Quadrillion Quintillion Sextillion Septillion Octillion Nonillion Decillion Undecillion Duodecillion Tredecillion Quattuordecillion Quindecillion Sexdecillion Septendecillion Octodecillion Novemdecillion Vigintillion Unvigintillion Duovigintillion Trevigintillion Quattuorvigintillion Quinvigintillion Sexvigintillion Septenvigintillion Octovigintillion Novemvigintillion Trigintillion Untrigintillion Duotrigintillion".split(" ");
 	var name = "";
-	if (n == 0) return "zero";
-	for (var i=groups;i>0;i--){
-		var tmp = " ";
-		var triplet = parseInt(data.substr((i-groups) * 3 - offset,(i==groups)?3-offset:3));
-		if (triplet > 0){
-		var hundreds = exports.getDigit(triplet,3);
-		var tens = exports.getDigit(triplet,2);
-		var ones = exports.getDigit(triplet,1);
-		var tenone = tens*10 + ones
-		
-		if (hundreds != 0) tmp += words[hundreds] + " hundred" + (tenone > 0) ? " and " : "";
-		if (tenone > 0){
-			if (tenone < 20) tmp += words[tenone];
-			else if (ones == 0) tmp +=  words[18+tens];
-			else tmp +=  words[18+tens] + "-" + words[ones];
-		}
-		if (i>1) tmp +=" " + words[27+i];
-		name += tmp;
-	}
-	}
-	return name;
+	n=n.toString();
+	if (n == "0") return "Zero";
+	var negative = (n[0] == '-');
+	var decI = n.indexOf(".");
+	var decimal = decI > -1 ? n.substr(decI) : '';
+	var number = n.substring(n.indexOf('-')+1,(decI > -1 ? decI - 1 : n.length));
 
+	var triplets = Math.ceil(number.length / 3);
+	
+	var offset = 3-number.length % 3; //final triplet offset
+	if (offset == 3) offset=0;
+	var out = [];
+	for (var i = 0; i < triplets; i++){
+		var cTrip;
+		if (i == 0 && offset > 0){
+			cTrip = number.substr(0,3-offset);
+		} else {
+			cTrip = number.substr(3*i - offset,3);
+		}
+		cTrip = parseInt(cTrip);
+		if (cTrip > 0){
+			var tmp = exports.tripletName(cTrip);
+			if (triplets-i-2 >= 0) tmp += " " + suffix[triplets-i-2];
+			out.push(tmp);
+		}
+	}
+	return out.join(", ").trim().toLowerCase();
 }
 
 exports.factorial = function(n){
