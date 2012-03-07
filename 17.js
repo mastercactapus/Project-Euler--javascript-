c09885db7d142c6b4ef5bb8a5b5f6aa7811eddf2
Nathan Caza <mastercactapus@gmail.com>
2012-03-07 17:42:20 -0600

    solved 17

---
+++ 17.js
@@ -0,0 +1,7 @@
+var numbers = require("./numbers");
+var sum=0;
+for (var i =1;i<=1000;i++){
+	var tmp = numbers.numberName(i).replace(/[ ,-]/g,"");
+	sum += numbers.numberName(i).replace(/[ ,-]/g,"").length;
+}
+console.log(sum);
