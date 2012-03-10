20fa50fdb98306d12c3e33959c1a429efaf633ab
Nathan Caza <mastercactapus@gmail.com>
2012-03-10 12:35:04 -0600

    solved 26

---
+++ 26.js
@@ -0,0 +1,11 @@
+var numbers = require("./numbers");
+var max=0;
+var size=0;
+for (var i=1;i<1000;i++){
+	var tmp=numbers.getDecimal(1,i);
+	if (tmp.length > size){
+		size = tmp.length;
+		max=i;
+	}
+}
+console.log(max);
