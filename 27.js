857d89f6a6fc72e722039a799d02d9b63d8987dd
Nathan Caza <mastercactapus@gmail.com>
2012-03-10 13:01:44 -0600

    solved 27

---
+++ 27.js
@@ -0,0 +1,19 @@
+var optimus= require("./optimus");
+var max=0;
+var max_prod=0;
+var calc = function(a,b,n){
+	return Math.pow(n,2) + a*n + b;
+}
+
+for (var a=-999;a<1000;a++){
+	for (var b=-999;b<1000;b++){
+		var i=0;
+		while (optimus.isPrime(calc(a,b,i))) i++;
+		if (i>max) {
+			max=i;
+			max_prod=a*b;
+		}
+	}
+}
+
+console.log(max_prod);
