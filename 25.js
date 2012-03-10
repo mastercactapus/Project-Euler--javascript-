862fd2c2b6d1fcbe5d7be76175b72806c9c98aa5
Nathan Caza <mastercactapus@gmail.com>
2012-03-10 12:34:58 -0600

    solved 25

---
+++ 25.js
@@ -0,0 +1,14 @@
+var bigint = require("bigint");
+var term=3;
+var f1=bigint(1);
+var f2=bigint(1);
+var fn=bigint(2);
+
+var max= bigint(10).pow(999);
+while (max.gt(fn)){
+	f1=f2;
+	f2=fn;
+	fn=f1.add(f2);
+	term++;
+}
+console.log(term);
