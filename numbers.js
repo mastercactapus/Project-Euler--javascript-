f69164463deb698611bff81a097bbdeab7a18022
Nathan Caza <nathaniel.caza@bestbuy.com>
2012-10-20 01:20:44 -0500

    did some lint-checking on numbers.js

--- numbers.js
+++ numbers.js
@@ -2,54 +2,54 @@ var digitStack=[];
 
 
 exports.countDigits = function(n){
-	if (digitStack.length==0)
+	if (digitStack.length===0)
 		for (var i=1;i<=20;i++)digitStack[i-1]=Math.pow(10,i);
 	var len=1;
 	for (var i=19;i>=0;i--)
 		if (n>digitStack[i]) return i+2;
 	return 1;
-}
+};
 
 exports.isPerfect = function(n){
 	return (exports.sumOfFactors(n) == n);
-}
+};
 exports.isAbundant = function(n){
 	return (exports.sumOfFactors(n) > n);
-}
+};
 exports.isDeficient = function(n){
 	return (exports.sumOfFactors(n) < n);
-}
+};
 exports.sumOfFactors = function(n){
 	return exports.arraySum(exports.getFactors(n));
-}
+};
 exports.isAmicable = function(n){
 	var partner = exports.sumOfFactors(n);
 	if (partner == n) return false;
-	if (exports.sumOfFactors(partner) == n) return true
+	if (exports.sumOfFactors(partner) == n) return true;
 	else return false;
-}
+};
 
 exports.getDecimal = function(n,d){
 	var out = [];
 	var carry = [];
 	var pattern_start=-1;
 	out.push(Math.floor(n/d));
-	if (n % d == 0) return out[0];
+	if (n % d === 0) return out[0];
 	carry.push(-1);
 	carry.push(n % d*10);
 	while (carry[carry.length-1] >0){
 		out.push(Math.floor(carry[carry.length-1] / d));
 		var c = carry[carry.length-1] % d;
-		if (c == 0) break;
+		if (c === 0) break;
 		pattern_start = carry.indexOf(c*10);
 		if (pattern_start > -1)break;
 		carry.push(c*10);
 	}
-	if (pattern_start > -1) 
+	if (pattern_start > -1)
 		return out[0] + '.' + out.slice(1,pattern_start).join("") + "(" + out.slice(pattern_start).join("") + ")";
 	else
 		return out[0] + "." + out.slice(1).join("");
-}
+};
 
 exports.lexiPermutation = function(str){
 	//swap two indexes
@@ -57,7 +57,7 @@ exports.lexiPermutation = function(str){
 		var t = str[x];
 		str[x]=str[y];
 		str[y]=t;
-	}
+	};
 	
 	var k=-1;
 	//this will find the least significant pair of numbers where
@@ -77,23 +77,23 @@ exports.lexiPermutation = function(str){
 	swap(k,l);
 	
 	//reverse the sequence for k+1 to the end of the array
-	// as k+1 is the most significant change, the others are 
+	// as k+1 is the most significant change, the others are
 	//already in greatest to least order.
   
 	return str.slice(0, k + 1).concat(str.slice(k + 1).reverse());
-}
+};
 
 //gets the nth digit of a number, starting at decimal (<=0 for decimal)
 exports.getDigit = function(n,digit){
 	return (Math.floor(n/(Math.pow(10,digit-1))))%10
-}
+};
 
 exports.arraySum = function(nArray){
 	var sum=0;
 	for (var i in nArray)
 		sum += nArray[i];
 	return sum;
-}
+};
 
 //tests if a number is a palindrome -- assumes integer
 exports.isPalindrome = function(n){
@@ -103,13 +103,13 @@ exports.isPalindrome = function(n){
 		if (str[i] != str[str.length-i-1]) return false;
 	}
 	return true;
-}
+};
 
 exports.isPythTriplet = function(a,b,c){
 	if (a >= b || b >= c) return false;
 	if (Math.pow(a,2) + Math.pow(b,2) == Math.pow(c,2)) return true;
 	else return false;
-}
+};
 
 exports.countFactors = function(n){
 	var factors=0;
@@ -119,7 +119,7 @@ exports.countFactors = function(n){
 	}
 	if (n/limit == limit) factors++;
 	return factors;
-}
+};
 
 exports.getFactors = function (n){
 	if (n <= 3){
@@ -133,18 +133,18 @@ exports.getFactors = function (n){
 	var factors=[1];
 	var limit = Math.floor(Math.sqrt(n));
 	for (var i=2;i<limit;i++){
-		if (n % i == 0) {
+		if (n % i === 0) {
 		factors.push(i);
 		factors.push(n/i);
-		};
+		}
 	}
 	if (n/limit == limit) factors.push(limit);
-	else if (n % limit == 0){
+	else if (n % limit === 0){
 		factors.push(limit);
 		factors.push(n/limit);
 	}
 	return factors;
-}
+};
 
 
 exports.stringSum = function(num1,num2){
@@ -160,33 +160,33 @@ exports.stringSum = function(num1,num2){
 		sum.push(tmp % 10);
 		carry = Math.floor(tmp/10);
 	}
-	while (sum[sum.length-1] == 0)sum.pop();
+	while (sum[sum.length-1] === 0)sum.pop();
 	return sum.reverse().join("");
 }
 
 exports.getDigit = function(n,digit){
 	n=parseInt(n);
-	return (Math.floor(n/(Math.pow(10,digit-1))))%10
-}
+	return (Math.floor(n/(Math.pow(10,digit-1))))%10;
+};
 exports.getDigitS = function(n,digit){ //todo
 	n=n.toString();
 
-}
+};
 exports.tripletName = function (n){
 	var words = "Zero One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen Twenty Thirty Forty Fifty Sixty Seventy Eighty Ninety Hundred".split(" ");
 	n = parseInt(n);
 	var h=exports.getDigit(n,3),
 		t=exports.getDigit(n,2),
 		o=exports.getDigit(n,1);
-	if (t+h+o == 0) return words[0];
+	if (t+h+o === 0) return words[0];
 	var out="";
-	if (h>0) {out += words[h] + " hundred" + ((t+o > 0) ? ' and':'')};
+	if (h>0) {out += words[h] + " hundred" + ((t+o > 0) ? ' and':'');}
 	
 	if (t>1) {out += " " + words[t + 18] + ((o>0) ? '-' + words[o] : '');}
 	else if (t == 1) {out += " " + words[o+10];}
-	else if (o>0){out += " " + words[o];};
+	else if (o>0){out += " " + words[o];}
 	return out.trim();
-}
+};
 
 exports.numberName= function (n){
 	var suffix = "Thousand Million Billion Trillion Quadrillion Quintillion Sextillion Septillion Octillion Nonillion Decillion Undecillion Duodecillion Tredecillion Quattuordecillion Quindecillion Sexdecillion Septendecillion Octodecillion Novemdecillion Vigintillion Unvigintillion Duovigintillion Trevigintillion Quattuorvigintillion Quinvigintillion Sexvigintillion Septenvigintillion Octovigintillion Novemvigintillion Trigintillion Untrigintillion Duotrigintillion".split(" ");
@@ -205,7 +205,7 @@ exports.numberName= function (n){
 	var out = [];
 	for (var i = 0; i < triplets; i++){
 		var cTrip;
-		if (i == 0 && offset > 0){
+		if (i === 0 && offset > 0){
 			cTrip = number.substr(0,3-offset);
 		} else {
 			cTrip = number.substr(3*i - offset,3);
@@ -218,7 +218,7 @@ exports.numberName= function (n){
 		}
 	}
 	return out.join(", ").trim().toLowerCase();
-}
+};
 
 exports.factorial = function(n){
 	var prod=1;
@@ -226,4 +226,4 @@ exports.factorial = function(n){
 		prod *= i;
 	}
 	return prod;
-}
+};
