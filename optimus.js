13f2b0fc9d8f4747be8bb79dc3ee638bef8d65fa
Nathan Caza <mastercactapus@gmail.com>
2012-10-27 02:01:22 -0400

    started work on quick prime searching

--- optimus.js
+++ optimus.js
@@ -1,3 +1,4 @@
+var util = require("./util");
 exports.primes =[2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
 73, 79, 83, 89, 97, 101, 103, 107, 109, 113,
@@ -18,7 +19,18 @@ var newPrime = function(){
 	return cTest;
 };
 
-
+//given n, return the next number that is prime
+exports.nextPrime = function(n) {
+    if (exports.primes[exports.primes.length-1] < n) {
+        while (exports.primes[exports.primes.length-1] < n) newPrime();
+        return exports.primes[exports.primes.length-1];
+    }
+    return util.scanArray(exports.primes, function(i) {
+        if (n == exports.primes[i] || (exports.primes[i]>n && exports.primes[i-1]<n)) return 0;
+        if (exports.primes[i] > n) return -1;
+        if (exports.primes[i] < n) return 1;
+    });
+}
 
 //tests if a number is prime
 exports.isPrime = function(n){
